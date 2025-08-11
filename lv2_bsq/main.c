25d5902a749e3ff6d9b47d1405a7cfd4eebe15d1
akyoshid <akyoshid@student.42tokyo.jp>
2025-08-11 19:33:05 +0000

    [FEAT] find_biggest_square()

--- lv2_bsq/main.c
+++ lv2_bsq/main.c
@@ -9,6 +9,10 @@ typedef struct s_data {
     char obstacle;
     char full;
     char **map;
+    int **dp_table;
+    int max_size;
+    int max_i;
+    int max_j;
 } t_data;
 
 FILE* get_fstream(char *filename) {
@@ -46,13 +50,13 @@ int is_1st_line_valid(t_data *d) {
         return 1;
 }
 
-char **calloc_map(t_data *d) {
-    char **tmp;
-    tmp = (char **)calloc(d->row + 1, sizeof(char *));
+int **calloc_dp_table(t_data *d) {
+    int **tmp;
+    tmp = (int **)calloc(d->row + 1, sizeof(int *));
     if (tmp == NULL)
         return NULL;
     for (int i = 0; i < d->row; ++i) {
-        tmp[i] = calloc(d->col + 1, sizeof(char));
+        tmp[i] = calloc(d->col + 1, sizeof(int));
         if (tmp[i] == NULL) {
             for (int j = 0; j < i; ++j)
                 free(tmp[j]);
@@ -63,10 +67,16 @@ char **calloc_map(t_data *d) {
     return tmp;
 }
 
-void free_map(char **map, t_data *d) {
+void free_map(t_data *d) {
     for (int i = 0; i < d->row; ++i)
-        free(map[i]);
-    free(map);
+        free(d->map[i]);
+    free(d->map);
+}
+
+void free_dp_table(t_data *d) {
+    for (int i = 0; i < d->row; ++i)
+        free(d->dp_table[i]);
+    free(d->dp_table);
 }
 
 // Success -> return 1
@@ -103,7 +113,7 @@ int read_map(FILE *f, t_data *d) {
     if (getline(&temp, &len, f) != -1 || errno != 0) {
         fprintf(stderr, "map error\n");
         free(temp);
-        free_map(d->map, d);
+        free_map(d);
         return -1;
     }
     return 0;
@@ -125,18 +135,85 @@ int set_data(FILE *f, t_data *d) {
     return 0;
 }
 
+void print_map(char **map) {
+    if (map == NULL)
+        return ;
+    for (int i = 0; map[i] != NULL; ++i)
+        fprintf(stdout, "%s", map[i]);
+}
+
+int get_min(int a, int b, int c) {
+    int min = a;
+    if (b < min)
+        min = b;
+    if (c < min)
+        min = c;
+    return min;
+}
+
+void fill_dp_table(t_data *d) {
+    for (int i = 0; i < d->row; ++i) {
+        for (int j = 0; j < d->col; ++j) {
+            if (d->map[i][j] == d->obstacle)
+                d->dp_table[i][j] = 0;
+            else if (i == 0 || j == 0)
+                d->dp_table[i][j] = 1;
+            else {
+                d->dp_table[i][j] = get_min(
+                        d->dp_table[i - 1][j - 1],
+                        d->dp_table[i - 1][j],
+                        d->dp_table[i][j - 1]) + 1;
+            }
+        }
+    }
+}
+
+void get_biggest_square(t_data *d) {
+    for (int i = 0; i < d->row; ++i) {
+        for (int j = 0; j < d->col; ++j) {
+            if (d->dp_table[i][j] > d->max_size) {
+                d->max_size = d->dp_table[i][j];
+                d->max_i = i - d->max_size + 1;
+                d->max_j = j - d->max_size + 1;
+            }
+        }
+    }
+}
+
+void rewrite_map(t_data *d) {
+    if (d->max_size == 0)
+        return ;
+    for (int i = 0; i < d->max_size; ++i) {
+        for (int j = 0; j < d->max_size; ++j)
+            d->map[d->max_i + i][d->max_j + j] = d->full;
+    }
+}
+
+void find_biggest_square(t_data *d) {
+    fill_dp_table(d);
+    get_biggest_square(d);
+    rewrite_map(d);
+}
+
 void bsq(char *filename) {
     FILE* f = get_fstream(filename);
     if (f == NULL)
         return ;
-    t_data d = {0, 0, 0, 0, 0, NULL};
+    t_data d = {0, 0, 0, 0, 0, NULL, NULL, 0, 0, 0};
     if (set_data(f, &d) == -1) {
         fclose(f);
         return ;
     }
-    //
-    free_map(d.map, &d);
     fclose(f);
+    d.dp_table = calloc_dp_table(&d);
+    if (d.dp_table == NULL) {
+        free_map(&d);
+        return ;
+    }
+    find_biggest_square(&d);
+    print_map(d.map);
+    free_map(&d);
+    free_dp_table(&d);
 }
 
 int main(int argc, char *argv[]) {
