7980235888575342e2797d8e651ef853c88dd2ff
akyoshid <akyoshid@student.42tokyo.jp>
2025-08-11 18:27:10 +0000

    [FEAT] set_data() & read_map()

---
+++ lv2_bsq/main.c
@@ -0,0 +1,150 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+
+typedef struct s_data {
+    int row;
+    int col;
+    char empty;
+    char obstacle;
+    char full;
+    char **map;
+} t_data;
+
+FILE* get_fstream(char *filename) {
+    FILE* f;
+    if (filename == NULL)
+        f = stdin;
+    else
+        f = fopen(filename, "r");
+    if (f == NULL)
+        fprintf(stderr, "map error\n");
+    return (f);
+}
+
+// Success -> return 1
+// Failure -> return 0
+int ft_isspace(int c) {
+    if (c >= 32 && c <= 126)
+        return 1;
+    else
+        return 0;
+}
+
+// Success -> return 1
+// Failure -> return 0
+int is_1st_line_valid(t_data *d) {
+    if (d->row <= 0
+        || ft_isspace(d->empty) == 0
+        || ft_isspace(d->obstacle) == 0
+        || ft_isspace(d->full) == 0
+        || d->empty == d->obstacle
+        || d->obstacle == d->full
+        || d->full == d->empty)
+        return 0;
+    else
+        return 1;
+}
+
+char **calloc_map(t_data *d) {
+    char **tmp;
+    tmp = (char **)calloc(d->row + 1, sizeof(char *));
+    if (tmp == NULL)
+        return NULL;
+    for (int i = 0; i < d->row; ++i) {
+        tmp[i] = calloc(d->col + 1, sizeof(char));
+        if (tmp[i] == NULL) {
+            for (int j = 0; j < i; ++j)
+                free(tmp[j]);
+            free(tmp);
+            return NULL;
+        }
+    }
+    return tmp;
+}
+
+void free_map(char **map, t_data *d) {
+    for (int i = 0; i < d->row; ++i)
+        free(map[i]);
+    free(map);
+}
+
+// Success -> return 1
+// Failure -> return 0
+int is_line_valid(char *line, t_data *d) {
+    for (int i = 0; i < d->col; ++i) {
+        if (!(line[i] == d->empty || line[i] == d->obstacle))
+            return 0;
+    }
+    if (line[d->col] != '\n')
+        return 0;
+    return 1;
+}
+
+// Success -> return 0
+// Failure -> return -1
+int read_map(FILE *f, t_data *d) {
+    int read = 0;
+    size_t len = 0;
+    for (int i = 0; i < d->row; ++i) {
+        read = getline(&(d->map[i]), &len, f);
+        if (i == 0)
+            d->col = read - 1;
+        if (read == -1 || d->col != read - 1 || is_line_valid(d->map[i], d) == 0) {
+            fprintf(stderr, "map error\n");
+            for (int j = 0; j <= i; ++j)
+                free(d->map[j]);
+            free(d->map);
+            return -1;
+        }
+    }
+    errno = 0;
+    char *temp = NULL;
+    if (getline(&temp, &len, f) != -1 || errno != 0) {
+        fprintf(stderr, "map error\n");
+        free(temp);
+        free_map(d->map, d);
+        return -1;
+    }
+    return 0;
+}
+
+// Success -> return 0
+// Failure -> return -1
+int set_data(FILE *f, t_data *d) {
+    if (fscanf(f, "%d %c %c %c\n", &d->row, &d->empty, &d->obstacle, &d->full) != 4
+        || is_1st_line_valid(d) == 0) {
+        fprintf(stderr, "map error\n");
+        return -1;
+    }
+    d->map = (char **)calloc(d->row + 1, sizeof(char *));
+    if (d->map == NULL)
+        return -1;
+    if (read_map(f, d) == -1)
+        return -1;
+    return 0;
+}
+
+void bsq(char *filename) {
+    FILE* f = get_fstream(filename);
+    if (f == NULL)
+        return ;
+    t_data d = {0, 0, 0, 0, 0, NULL};
+    if (set_data(f, &d) == -1) {
+        fclose(f);
+        return ;
+    }
+    //
+    free_map(d.map, &d);
+    fclose(f);
+}
+
+int main(int argc, char *argv[]) {
+    if (argc == 1)
+        bsq(NULL);
+    else {
+        for (int i = 1; i < argc; ++i)
+            bsq(argv[i]);
+    }
+    return 0;
+}
