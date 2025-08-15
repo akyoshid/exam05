f6aa0519cf77b1ed60f526ee488017f52cc4ddbd
akyoshid <akyoshid@student.42tokyo.jp>
2025-08-15 08:18:58 +0000

    [FIX] main.cpp in subjects

--- lv1_bigint/subjects/main.cpp
+++ lv1_bigint/subjects/main.cpp
@@ -36,14 +36,14 @@ int	main(void)
 		std::cout << "(d > a) = " << (d > a) << std::endl;
 		std::cout << "(d == a) = " << (d == a) << std::endl;
 		std::cout << "(d != a) = " << (d != a) << std::endl;
-		std::cout << "(d == a) = " << (d == d) << std::endl;
+		std::cout << "(d == d) = " << (d == d) << std::endl;
 		std::cout << "(d <= a) = " << (d <= a) << std::endl;
 		std::cout << "(d >= a) = " << (d >= a) << std::endl;
 
 		bigint x(1234);
 		std::cout << std::endl;
 		std::cout << "\"(x << 2)\"--> " << (x << 2) << "\n";   // prints 123400
-		std::cout << "\"(x >>= 2)\"--> " << (x >>= 2) << "\n";   // prints   12}
+		std::cout << "\"(x >>= 2)\"--> " << (x >>= 2) << "\n";   // prints   12
 	}
 	return (0);
 }
