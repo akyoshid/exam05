162c6cbf1825ceeae50958734500008b03fc05d4
akyoshid <akyoshid@student.42tokyo.jp>
2025-08-15 07:28:36 +0000

    [FEAT] constructor, operator +, +=

---
+++ lv1_bigint/round2/bigint.cpp
@@ -0,0 +1,59 @@
+#include <string>
+#include <algorithm>
+#include "bigint.hpp"
+
+bigint::bigint() : digit("0") {
+}
+
+bigint::bigint(unsigned long long num) : digit(std::to_string(num)) {
+}
+
+bigint::bigint(const bigint& src) : digit(src.digit) {
+}
+
+bigint& bigint::operator=(const bigint& src) {
+    if (this != &src)
+        digit = src.digit;
+    return *this;
+}
+
+bigint::~bigint() {
+}
+
+bigint bigint::operator+(const bigint& rhs) const {
+    std::string result = "";
+    int l_len = this->digit.length();
+    int r_len = rhs.digit.length();
+    int carry = 0;
+    for (int i = 0; (i < l_len || i < r_len); ++i) {
+        int sum = carry;
+        if (i < l_len)
+            sum += this->digit[l_len - i - 1] - '0';
+        if (i < r_len)
+            sum += rhs.digit[r_len - i - 1] - '0';
+        result.push_back('0' + sum % 10);
+        carry = sum / 10;
+    }
+    if (carry > 0)
+        result.push_back('0' + carry);
+    std::reverse(result.begin(), result.end());
+    return bigint(result);
+}
+
+bigint& bigint::operator+=(const bigint& rhs) {
+    *this = *this + rhs;
+    return *this;
+}
+
+std::string bigint::get_digit() const {
+    return digit;
+}
+
+
+bigint::bigint(const std::string& str) : digit(str) {
+}
+
+std::ostream& operator<<(std::ostream& lhs, const bigint& rhs) {
+    lhs << rhs.get_digit();
+    return lhs;
+}
---
+++ lv1_bigint/round2/bigint.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <string>
+#include <iostream>
+
+class bigint {
+ public:
+    bigint();
+    bigint(unsigned long long num);
+    bigint(const bigint& src);
+    bigint& operator=(const bigint& src);
+    ~bigint();
+    bigint operator+(const bigint& rhs) const;
+    bigint& operator+=(const bigint& rhs);
+    std::string get_digit() const;
+ private:
+    bigint(const std::string& str);
+    std::string digit;
+};
+
+std::ostream& operator<<(std::ostream& lhs, const bigint& rhs);
