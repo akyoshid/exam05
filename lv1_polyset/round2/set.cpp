25b603fe6df4a1e36d3839d4cadadfac18f719f3
akyoshid <akyoshid@student.42tokyo.jp>
2025-08-16 07:42:55 +0000

    [FEAT] set class

---
+++ lv1_polyset/round2/set.cpp
@@ -0,0 +1,43 @@
+#include "set.hpp"
+
+set::set(searchable_bag &bag) : bag(bag) {
+}
+
+set::set(const set& src) : bag(src.bag) {
+}
+
+set& set::operator=(const set& src) {
+    if (this != &src)
+        bag = src.bag;
+    return *this;
+}
+
+set::~set() {
+}
+
+void set::insert(int item) {
+    if (has(item) == false)
+        bag.insert(item);
+}
+
+void set::insert(int* items, int count) {
+    for (int i = 0; i < count; ++i) {
+        insert(items[i]);
+    }
+}
+
+void set::print() const {
+    bag.print();
+}
+
+void set::clear() {
+    bag.clear();
+}
+
+bool set::has(int item) const {
+    return bag.has(item);
+}
+
+const searchable_bag& set::get_bag() const {
+    return bag;
+}
